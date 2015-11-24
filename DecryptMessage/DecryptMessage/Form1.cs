441073974ba4cf3686fddd8131c05bb023e3d10e
Matheus Monte <mmonte@indt.local>
2015-11-24 15:10:04 -0400

    Add project to decrypt message on the chat interference
    Signed-off-by: Matheus Monte <mmonte@indt.local>

---
+++ DecryptMessage/DecryptMessage/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecryptMessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Decrypt(messageText.Text, GenerateNewKey()));
+        }
+
+
+        private static string GenerateNewKey()
+        {
+            byte[] inArray = new byte[32];
+
+            for (int x = 0; x < inArray.Length; x++)
+            {
+                inArray[x] = (byte)x;
+            }
+
+            return Convert.ToBase64String(inArray);
+        }
+        private static byte[] bIV =
+               { 0x50, 0x08, 0xF1, 0xDD, 0xDE, 0x3C, 0xF2, 0x18,
+                0x44, 0x74, 0x19, 0x2C, 0x53, 0x49, 0xAB, 0xBC };
+
+
+        public static string Decrypt(string text, string cryptoKey)
+        {
+
+            try
+            {
+                // Se a string não está vazia, executa a criptografia           
+                if (!string.IsNullOrEmpty(text))
+                {
+                    // Cria instancias de vetores de bytes com as chaves                
+                    byte[] bKey = Convert.FromBase64String(cryptoKey);
+                    byte[] bText = Convert.FromBase64String(text);
+
+                    // Instancia a classe de criptografia Rijndael                
+                    Rijndael rijndael = new RijndaelManaged();
+
+                    // Define o tamanho da chave "256 = 8 * 32"                
+                    // Lembre-se: chaves possíves:                
+                    // 128 (16 caracteres), 192 (24 caracteres) e 256 (32 caracteres)                
+                    rijndael.KeySize = 256;
+
+                    // Cria o espaço de memória para guardar o valor DEScriptografado:               
+                    MemoryStream mStream = new MemoryStream();
+
+                    // Instancia o Decriptador                 
+                    CryptoStream decryptor = new CryptoStream(
+                        mStream,
+                        rijndael.CreateDecryptor(bKey, bIV),
+                        CryptoStreamMode.Write);
+
+                    // Faz a escrita dos dados criptografados no espaço de memória   
+                    decryptor.Write(bText, 0, bText.Length);
+                    // Despeja toda a memória.                
+                    decryptor.FlushFinalBlock();
+                    // Instancia a classe de codificação para que a string venha de forma correta         
+                    UTF8Encoding utf8 = new UTF8Encoding();
+
+                    return utf8.GetString(mStream.ToArray());
+                }
+                else
+                {
+                    // Se a string for vazia retorna nulo                
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Se algum erro ocorrer, dispara a exceção            
+                throw new ApplicationException("Erro ao descriptografar", ex);
+            }
+        }
+
+    }
+}
